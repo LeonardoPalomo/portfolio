15a710918578afc276e9ba2c1c93f9b9809e71e0
LeonardoPalomo <leonardo.palomo@usach.cl>
2024-03-01 14:27:00 -0300

    Add logic (javascript) to site

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Showcase of projects I have worked on.
 
 Live preview at: https://leonardopalomo.github.io/portfolio/
+
+TO DO:
+- Fix the resizing sidebar when the contents of the description is too much.
--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,43 @@
-console.log("hello world");
+const welcomeContainer = document.querySelector('#welcome');
+
+//---------------------------NEURONE LOGIC ------------------------- 
+const neuroneLink = document.querySelector('#neurone');
+const neuroneDetails = document.querySelector('#neurone-details');
+
+neuroneLink.addEventListener('mouseenter', (e) => {
+  welcomeContainer.setAttribute("hidden", "");
+  neuroneDetails.removeAttribute("hidden");
+});
+
+neuroneLink.addEventListener('mouseout', (e) => {
+  welcomeContainer.removeAttribute("hidden");
+  neuroneDetails.setAttribute("hidden", "");
+});
+
+//---------------------SYSMEC LOGIC ----------------------------- 
+const sysmecLink = document.querySelector('#sysmec');
+const sysmecDetails = document.querySelector('#sysmec-details');
+
+sysmecLink.addEventListener('mouseenter', (e) => {
+  welcomeContainer.setAttribute("hidden", "");
+  sysmecDetails.removeAttribute("hidden");
+});
+
+sysmecLink.addEventListener('mouseout', (e) => {
+  welcomeContainer.removeAttribute("hidden");
+  sysmecDetails.setAttribute("hidden", "");
+});
+
+//----------------------ETCH A SKETCH LOGIC ----------------------------- 
+const etchasketchLink = document.querySelector('#etchasketch');
+const etchasketchDetails = document.querySelector('#etchasketch-details');
+
+etchasketchLink.addEventListener('mouseenter', (e) => {
+  welcomeContainer.setAttribute("hidden", "");
+  etchasketchDetails.removeAttribute("hidden");
+});
+
+etchasketchLink.addEventListener('mouseout', (e) => {
+  welcomeContainer.removeAttribute("hidden");
+  etchasketchDetails.setAttribute("hidden", "");
+});
