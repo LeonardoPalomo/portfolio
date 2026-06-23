67817cf94769b99c7e51cc9cdaea08f9d7c96ea3
Leonardo Palomo <leonardo.palomo@usach.cl>
2026-06-23 03:16:00 -0400

    cambio a español y actualización

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,5 @@
+
+
 const welcomeContainer = document.querySelector('#welcome');
 let activeDescription = welcomeContainer;
 
@@ -22,22 +24,53 @@ sysmecLink.addEventListener('mouseenter', (e) => {
 });
 
 //----------------------ETCH A SKETCH LOGIC ----------------------------- 
-const etchasketchLink = document.querySelector('#etchasketch');
-const etchasketchDetails = document.querySelector('#etchasketch-details');
+// const etchasketchLink = document.querySelector('#etchasketch');
+// const etchasketchDetails = document.querySelector('#etchasketch-details');
 
-etchasketchLink.addEventListener('mouseenter', (e) => {
+// etchasketchLink.addEventListener('mouseenter', (e) => {
+//   activeDescription.setAttribute("hidden", "");
+//   activeDescription = etchasketchDetails;
+//   activeDescription.removeAttribute("hidden");
+// });
+
+//---------------------HUECHURABA LOGIC ----------------------------- 
+const huechurabaLink = document.querySelector('#huechuraba');
+const huechurabaDetails = document.querySelector('#huechuraba-details');
+
+huechurabaLink.addEventListener('mouseenter', (e) => {
   activeDescription.setAttribute("hidden", "");
-  activeDescription = etchasketchDetails;
+  activeDescription = huechurabaDetails;
   activeDescription.removeAttribute("hidden");
 });
+
+//---------------------HUASOVENTURAS LOGIC ----------------------------- 
+const huasoventurasLink = document.querySelector('#huasoventuras');
+const huasoventurasDetails = document.querySelector('#huasoventuras-details');
+
+huasoventurasLink.addEventListener('mouseenter', (e) => {
+  activeDescription.setAttribute("hidden", "");
+  activeDescription = huasoventurasDetails;
+  activeDescription.removeAttribute("hidden");
+});
+
+//---------------------LA ULTIMA SOPAIPILLA (LUS) LOGIC ----------------------------- 
+const lusLink = document.querySelector('#lus');
+const lusDetails = document.querySelector('#lus-details');
+
+lusLink.addEventListener('mouseenter', (e) => {
+  activeDescription.setAttribute("hidden", "");
+  activeDescription = lusDetails;
+  activeDescription.removeAttribute("hidden");
+});
+
 
 //------------------CURRENT PROJECTS ----------------------------------
-const currentLink = document.querySelector('#current');
-const currentDetails = document.querySelector('#current-details');
+// const currentLink = document.querySelector('#current');
+// const currentDetails = document.querySelector('#current-details');
 
-currentLink.addEventListener('mouseenter', (e) => {
-  activeDescription.setAttribute("hidden", "");
-  activeDescription = currentDetails;
-  activeDescription.removeAttribute("hidden");
-});
+// currentLink.addEventListener('mouseenter', (e) => {
+//   activeDescription.setAttribute("hidden", "");
+//   activeDescription = currentDetails;
+//   activeDescription.removeAttribute("hidden");
+// });
 
